8d42927304a2439351b4371fd39adb5283c38402
Rishikesh <DarnuleRishikesh@gmail.com>
2026-04-09 18:46:27 +0530

    minor UI changes

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,18 +5,15 @@ export default function loading() {
   return (
     <>
       <div className="h-[100vh] w-[100vw] bg-black flex flex-col items-center justify-center">
-        <Navbar />
+        {/* <Navbar /> */}
         <section className="Spotlight_Skeleton">
           <div className="SpotlightInfo_Wrapper">
-            <div className="SpotlightPoster_Wrapper">
-              <div id="SpotlightPoster_Skeleton"> </div>
+            <div className="SpotlightDescriptionline">
               <div id="SpotlightPoster_Title"> </div>
+              <div id="line1"> </div>
+              <div id="line2"> </div>
+              <div id="line3"> </div>
             </div>
-              <div className="SpotlightDescriptionline">
-                <div id="line1"> </div>
-                <div id="line2"> </div>
-                <div id="line3"> </div>
-              </div>
           </div>
         </section>
       </div>
